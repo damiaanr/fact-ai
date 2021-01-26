Fixed clip gradient to be more like scvis

main.py
@@ -44,12 +44,12 @@ print('Loading datasets...')
 # Loading all datasets - tuples: (X, num_clusters)
 datasets = {
   #'Bipolar': (load_data(global_dir + '/data/Bipolar.tsv', False, False, False)[0], 18),
-  #'Housing': (load_data(global_dir + '/data/Housing.tsv', False, False, False)[0], 6),
-  #'Iris': (load_data(global_dir + '/data/Iris.tsv', False, False, False)[0], 3),
+  'Housing': (load_data(global_dir + '/data/Housing.tsv', False, False, False)[0], 6),
+  'Iris': (load_data(global_dir + '/data/Iris.tsv', False, False, False)[0], 3),
   'Heart': (load_data(global_dir + '/data/Heart.tsv', False, False, False)[0], 8),
-  ##'Seeds': (load_data(global_dir + '/data/Seeds.tsv', False, True, True)[0], 3),
+  'Seeds': (load_data(global_dir + '/data/Seeds.tsv', False, True, True)[0], 3),
   'Wine': (load_data(global_dir + '/data/Wine.tsv', False, True, False)[0], 3),
-  ##'Glass': (load_data(global_dir + '/data/Glass.tsv', False, True, True)[0], 7),
+  'Glass': (load_data(global_dir + '/data/Glass.tsv', False, True, True)[0], 7),
 }
 
 # Preload 
@@ -74,7 +74,7 @@ for dataset in datasets.keys():
   
     transformer = transform_functions[dr_algorithm]
     latent_X    = transformer(original_X)
-    
+
     latent_X[~np.isfinite(latent_X)] = 0
   
     # Generate clusters in latent space (K-means, but could be anything)
@@ -134,7 +134,7 @@ for dataset in datasets.keys():
     axs[-1, -1].axis('off')
     if len(transform_functions) > 6:
       axs[-2, -1].axis('off')
-      
+
   fig.suptitle(dataset + " dataset (variance adjusted to " + str(variance_adjust) + ", " + str(num_clusters) + " clusters)")
   plt.show()
   fig.savefig(global_dir + "/results/plots/latent_spaces_" + dataset + "_" + str(datetime.timestamp(datetime.now())) + ".png", bbox_inches='tight')

vae/encoder.py
@@ -159,7 +159,7 @@ class CustomLoss(nn.Module):
 
         # Computes half the L2 norm of a tensor without the `sqrt`: output = sum(t ** 2) / 2
         # Converted from tf.nn.l2_loss
-        penalty = [torch.sum(var ** 2) / 2 for name, var in self._net.named_parameters() if 'weight' in name]
+        penalty = [torch.sum(var ** 2) / 2 for name, var in self._net.named_parameters() if '.weight' in name]
         l2_regularizer = L2_REGULARISATION * sum(penalty)
         return l2_regularizer
 

vae/train.py
@@ -7,7 +7,8 @@ import datetime
 
 MIN_ITER = 3000
 MAX_ITER = 30000
-
+CLIP_VALUE = 3.0
+CLIP_NORM = 10.0
 torch.manual_seed(42)
 
 def trainVAE(x, global_dir, dataset):
@@ -54,8 +55,14 @@ def trainVAE(x, global_dir, dataset):
       # loss.backward() computes the derivative of the loss w.r.t. the parameters (or anything requiring gradients) using backpropagation.
       loss.backward()
 
-      # TODO clip_gradient like in scvis
-      torch.nn.utils.clip_grad_norm_(loss, max_norm=10.0)
+      # Clip gradients like in SCVIS
+      torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=CLIP_NORM)
+      for name, var in net.named_parameters():
+          if 'encoder_layer_sigma_square' in name:
+              torch.clamp(var.grad, -CLIP_VALUE*0.1, CLIP_VALUE*0.1)
+          else:
+              torch.clamp(var.grad, -CLIP_VALUE, CLIP_VALUE)
+
 
       # optimizer.step() causes the optimizer to take a step based on the gradients of the parameters.
       optimizer.step()